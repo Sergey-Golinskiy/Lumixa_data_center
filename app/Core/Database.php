1b624a43bf4c69edcace3e84c4596b40717e8152
GOLINSKIY <sngolinskiy@gmail.com>
2026-01-17 10:49:37 +0200

    Enable buffered MySQL queries

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -51,6 +51,7 @@ class Database
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$charset} COLLATE {$charset}_unicode_ci"
         ];
 
