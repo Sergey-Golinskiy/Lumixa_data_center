26dd0691d146dddfb63d0845d50beff3a7597e1f
GOLINSKIY <sngolinskiy@gmail.com>
2026-01-17 23:56:09 +0200

    Ensure printers id type before FK

--- storage/migrations/2024_01_01_000017_add_printer_to_details.sql
+++ storage/migrations/2024_01_01_000017_add_printer_to_details.sql
@@ -2,6 +2,9 @@
 -- Lumixa LMS - Link details to printers
 -- =====================================================
 
+ALTER TABLE printers
+    MODIFY COLUMN id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE details
     ADD COLUMN printer_id BIGINT UNSIGNED NULL AFTER material_item_id,
     ADD INDEX idx_printer_id (printer_id),
